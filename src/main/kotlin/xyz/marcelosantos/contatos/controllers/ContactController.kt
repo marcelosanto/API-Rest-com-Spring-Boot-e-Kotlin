5647a68e9ba0603c6338d06fc1079ba33d4f2c6c
Marcelo Santos <rocha.mer21@gmail.com>
2021-04-23 10:43:37 -0300

    criado o Get, Post e Put

--- src/main/kotlin/xyz/marcelosantos/contatos/controllers/ContactController.kt
+++ src/main/kotlin/xyz/marcelosantos/contatos/controllers/ContactController.kt
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.*
 import xyz.marcelosantos.contatos.entities.Contact
 import xyz.marcelosantos.contatos.repositories.ContactRepository
+import javax.persistence.EntityNotFoundException
 
 @RestController
 @RequestMapping("/contacts")
@@ -18,6 +19,18 @@ class ContactController {
 
     @PostMapping
     fun create(@RequestBody contact: Contact): Contact {
+        return repository.save(contact)
+    }
+
+    @PutMapping("/{id}")
+    fun update(@PathVariable("id") id: Long, @RequestBody newContact: Contact): Contact{
+        val contact = repository.findById(id).orElseThrow { EntityNotFoundException() }
+
+        contact.apply {
+            this.name = newContact.name
+            this.email = newContact.email
+        }
+
         return repository.save(contact)
     }
 }
