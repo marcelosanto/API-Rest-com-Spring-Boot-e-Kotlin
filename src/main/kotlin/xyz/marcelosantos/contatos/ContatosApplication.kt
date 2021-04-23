8b2acf7a29366c8cbe1354954fedff863ae444f3
Marcelo Santos <rocha.mer21@gmail.com>
2021-04-23 09:46:52 -0300

    iniciando

---
+++ src/main/kotlin/xyz/marcelosantos/contatos/ContatosApplication.kt
@@ -0,0 +1,11 @@
+package xyz.marcelosantos.contatos
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ContatosApplication
+
+fun main(args: Array<String>) {
+	runApplication<ContatosApplication>(*args)
+}
